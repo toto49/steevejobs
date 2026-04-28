187731dcdf8a8000001f22f9cd22f60dd49a6c00
atom497 <tomboudaud0105@gmail.com>
2026-04-28 15:42:13 +0200

    feat : ajout de la page de connexion
    Signed-off-by: atom497 <tomboudaud0105@gmail.com>

--- src/main/java/com/eseo/steevejobs/Launcher.java
+++ src/main/java/com/eseo/steevejobs/Launcher.java
@@ -1,9 +1,7 @@
 package com.eseo.steevejobs;
 
-import javafx.application.Application;
-
 public class Launcher {
-    public static void main(String[] args) {
-        Application.launch(HelloApplication.class, args);
-    }
+   public static void main(String[] args) {
+      HelloApplication.main(args);
+   }
 }
