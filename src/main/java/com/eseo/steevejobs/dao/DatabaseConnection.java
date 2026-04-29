eed2ce156e07f16e9c4cf2d580155aa8af686652
remig <remi.goubin@outlook.com>
2026-04-29 10:33:57 +0200

    feat: Database connexion
    Création de la Database connexion, configuration de l'appel du .env et rajout des dépendances nécessaire

---
+++ src/main/java/com/eseo/steevejobs/dao/DatabaseConnection.java
@@ -0,0 +1,19 @@
+package com.eseo.steevejobs.dao;
+
+import io.github.cdimascio.dotenv.Dotenv;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class DatabaseConnection {
+    static Dotenv dotenv = Dotenv.configure().directory("./").load();
+
+    private static final String URL = dotenv.get("DB_URL");
+    private static final String USER = dotenv.get("DB_USER");
+    private static final String PASSWORD = dotenv.get("DB_PASSWORD");
+
+    public static Connection getConnection() throws SQLException {
+        return DriverManager.getConnection(URL, USER, PASSWORD);
+    }
+}
