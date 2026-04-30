dcd3e3c5c37c3b809cee1ded5e6f88e07ab2ce8e
morgiane <taleb.morgiane@gmail.com>
2026-04-30 10:20:24 +0200

    test(dao): add UserDAOTest Issue #SCRUM-38

---
+++ src/test/java/com/eseo/steevejobs/dao/UserDAOTest.java
@@ -0,0 +1,11 @@
+package com.eseo.steevejobs.dao;
+
+import com.eseo.steevejobs.model.User;
+
+import java.sql.*;
+import java.util.ArrayList;
+import java.util.List;
+
+public class UserDAOTest {
+
+}
